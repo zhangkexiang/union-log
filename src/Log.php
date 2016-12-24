6c2d1bf1584ff859da61c37ca78b21b35f46e7e1
zhangkexiang <59916375@qq.com>
2016-12-25 00:05:14 +0800

    update

--- src/Log.php
+++ src/Log.php
@@ -15,22 +15,24 @@ class Log
     {
         $this->monolog = new Logger($name);
     }
-
-    public function info()
+    //声明成私有方法 外部通过静态调用则访问不到该方法进入 __callStatic 方法 而 callstatic方法中可以调用到private方法
+    private function info($path,$msg)
     {
-
-        $this->monolog->pushHandler($handler = new StreamHandler('/tmp/laravel.log', Logger::INFO));
-
+        $this->monolog->pushHandler($handler = new StreamHandler($path, Logger::INFO));
         $handler->setFormatter(new LineFormatter(
                 "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n",
                 "Y-m-d H:i:s",
                 false,
                 false
         ));
-        $this->monolog->info("222",[]);
+        $this->monolog->info($msg,[]);
 
     }
 
+    public static function __callStatic($method,$parameters){
+//        call_user_func_array([new self, $method], $parameters);
+        call_user_func_array([new static, $method], $parameters);
+    }
 
 }
 
--- src/test.php
+++ src/test.php
@@ -2,5 +2,6 @@
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/Log.php';
 use Union\Log;
-$info = new Log();
-$info->info();
+//$info = new Log();
+//$info->info();
+Log::info('/tmp/laravel.log','asdf');
